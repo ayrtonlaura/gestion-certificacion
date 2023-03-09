d3dfadf72a14c0e334c09ad7590df3359723e874
Ayrton Laura <ayrtonlauracaja@gmail.com>
2023-03-08 21:16:29 -0500

    first commit balanza

---
+++ src/app/balanza/balanza.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-balanza',
+  templateUrl: './balanza.component.html',
+  styleUrls: ['./balanza.component.css']
+})
+export class BalanzaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
